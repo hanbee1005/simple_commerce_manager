693c60b06bfd3d1fa07ac36797034871558b2220
hson <hson@hist.co.kr>
2020-06-12 17:24:22 +0900

    scm-main 모듈 추가 및 app 모듈 내 컴포넌트 정리

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,21 +2,27 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { NavbarComponent } from './navbar/navbar.component';
-import { SidebarComponent } from './sidebar/sidebar.component';
-import { FooterComponent } from './footer/footer.component';
-import {FormsModule} from "@angular/forms";
+
+import { FormsModule } from "@angular/forms";
+
+// 직접 생성한 모듈
+import { ProductModule } from "./product/product.module";
+import { CategoryModule } from "./category/category.module";
+
+import { ScmMainModule } from "./scm-main/scm-main.module";
 
 @NgModule({
   declarations: [
-    AppComponent,
-    NavbarComponent,
-    SidebarComponent,
-    FooterComponent
+    AppComponent
   ],
     imports: [
+        /* Angular Modules */
         BrowserModule,
-        FormsModule
+        FormsModule,
+        /* App Modules */
+        ProductModule,
+        CategoryModule,
+        ScmMainModule
     ],
   providers: [],
   bootstrap: [AppComponent]
