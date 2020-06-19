65c7923989b3b2f0e08ca729f636f8f2bf04fdf3
hanbee <sonhanbi1002@gmail.com>
2020-06-19 13:31:15 +0900

    product-management, category-management 컴포넌트 생성 및 app 컴포넌트에 추가

--- src/app/category/category.module.ts
+++ src/app/category/category.module.ts
@@ -1,12 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { CategoryManagementComponent } from './category-management/category-management.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [CategoryManagementComponent],
   imports: [
     CommonModule
+  ],
+  exports: [
+    CategoryManagementComponent
   ]
 })
 export class CategoryModule { }
--- src/app/product/product.module.ts
+++ src/app/product/product.module.ts
@@ -1,12 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ProductManagementComponent } from './product-management/product-management.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [ProductManagementComponent],
   imports: [
     CommonModule
+  ],
+  exports: [
+    ProductManagementComponent
   ]
 })
 export class ProductModule { }
