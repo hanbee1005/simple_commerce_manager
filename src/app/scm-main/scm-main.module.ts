58e45f84ad723d34ea1b93ab75389e7cd0fedad5
hanbee <sonhanbi1002@gmail.com>
2020-06-19 13:33:36 +0900

    main-dashboard, page-not-found 컴포넌트 생성

--- src/app/scm-main/scm-main.module.ts
+++ src/app/scm-main/scm-main.module.ts
@@ -4,12 +4,16 @@ import { CommonModule } from '@angular/common';
 import { NavbarComponent } from './navbar/navbar.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
 import { FooterComponent } from './footer/footer.component';
+import { MainDashboardComponent } from './main-dashboard/main-dashboard.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 @NgModule({
   declarations: [
     NavbarComponent,
     SidebarComponent,
-    FooterComponent
+    FooterComponent,
+    MainDashboardComponent,
+    PageNotFoundComponent
   ],
   imports: [
     CommonModule
